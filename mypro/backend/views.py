281c46b131f1537dc4d1302ce0dcab739f32ab26
diya1511 <diyaagrawal1511@gmail.com>
2021-08-18 00:32:54 +0530

    ok

---
+++ mypro/backend/views.py
@@ -0,0 +1,18 @@
+
+from django.http.response import HttpResponseNotAllowed
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request,"index.html")
+ 
+def about(request) :
+    return render(request,"about.html")
+    
+def contact(request) :
+    return render(request,"contact.html")
+   
+def services(request) :
+    return render(request,"services.html")
+   
