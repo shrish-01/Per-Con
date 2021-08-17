da1b87429e742cdbbd623bd2b988bcb737f5c932
diya1511 <diyaagrawal1511@gmail.com>
2021-08-17 21:40:31 +0530

    done

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MacConfig(AppConfig):
+class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MAC'
+    name = 'App'
