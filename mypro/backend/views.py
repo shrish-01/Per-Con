c10d85216604f00e70c5f92e70ff93877a056f48
diya1511 <diyaagrawal1511@gmail.com>
2021-08-18 01:13:15 +0530

    ok

--- mypro/backend/views.py
+++ mypro/backend/views.py
@@ -2,7 +2,7 @@
 from django.http.response import HttpResponseNotAllowed
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 
 def home(request):
     return render(request,"index.html")
