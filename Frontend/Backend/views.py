bce78cddeede411b448badea2779ff0c3591ad1d
diya1511 <diyaagrawal1511@gmail.com>
2021-08-18 12:25:07 +0530

    ok

--- Frontend/Backend/views.py
+++ Frontend/Backend/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from .models import *
 
 def home(request):
-    return render(request,"index.html")
+    return render(request,"home.html")
  
 def about(request) :
     return render(request,"about.html")
