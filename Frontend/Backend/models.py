34a28f3b74c8fe503015f12ad54a228155378ceb
diya1511 <diyaagrawal1511@gmail.com>
2021-08-18 14:26:36 +0530

    .

--- Frontend/Backend/models.py
+++ Frontend/Backend/models.py
@@ -1,2 +1,7 @@
 from django.db import models
-
+class Contact(models.Model):
+    name=models.CharField(max_length=120)
+    email=models.CharField(max_length=120)
+    phone=models.CharField(max_length=120)
+    desc=models.TextField()
+    date=models.DateField()
