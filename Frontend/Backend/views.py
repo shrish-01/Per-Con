1dc11334949e7a3da531a4302f24296091d90d99
diya1511 <diyaagrawal1511@gmail.com>
2021-08-18 19:19:36 +0530

    model done

--- Frontend/Backend/views.py
+++ Frontend/Backend/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http.response import HttpResponseNotAllowed
 from django.http import HttpResponse
 from .models import *
+from datetime import datetime
 
 def home(request):
     return render(request,"home.html")
@@ -10,8 +11,15 @@ def about(request) :
     return render(request,"about.html")
     
 def contact(request) :
+    if request.method == 'POST' :
+     name = request.POST.get('name')
+     email= request.POST.get('email')
+     phone = request.POST.get('phone')
+     desc = request.POST.get('desc')
+    
+    contact =Contact(name=name,email=email,phone=phone,desc=desc,date= datetime.today())
+    contact.save()
     return render(request,"contact.html")
-   
 def services(request) :
     return render(request,"services.html")
    
