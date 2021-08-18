7ffb56e0cb443489d77a70d4fc7521e8c278ce2b
diya1511 <diyaagrawal1511@gmail.com>
2021-08-18 20:30:40 +0530

    ok

--- Frontend/Backend/urls.py
+++ Frontend/Backend/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from Backend import views
+from . import views
 urlpatterns = [
     path('',views.home, name='home') ,
-    path('about',views.about, name='about') ,
-    path('contact',views.contact, name='contact') ,
-    path('services',views.services, name='services') ,
+    path('about/',views.about, name='about') ,
+    path('contact/',views.contact, name='contact') ,
+    path('services/',views.services, name='services') ,
 ]
