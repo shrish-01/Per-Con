ce8c7b8812d07a7743f2e92f99cacb6e50c57a52
23-spurtiNarkhedkar <86821974+23-spurtiNarkhedkar@users.noreply.github.com>
2021-08-20 20:57:04 +0530

    done

--- Frontend/Backend/models.py
+++ Frontend/Backend/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 class Contact(models.Model):
-    name=models.CharField(max_length=120)
+    fname=models.CharField(max_length=120)
+    lname=models.CharField(max_length=120)
+    sub=models.CharField(max_length=500)
     email=models.CharField(max_length=120)
     phone=models.CharField(max_length=120)
     desc=models.TextField()
