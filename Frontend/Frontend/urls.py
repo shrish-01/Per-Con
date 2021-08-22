2d05e070b894f5fecbe3c8a9897fdc4731ce5adc
diya1511 <diyaagrawal1511@gmail.com>
2021-08-22 09:32:38 +0530

    ok

--- Frontend/Frontend/urls.py
+++ Frontend/Frontend/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+
 admin.site.site_header = "PERCON"
 admin.site.site_title = "PERCON Admin Portal"
 admin.site.index_title = "Welcome to PERCON Portal"
@@ -22,5 +23,5 @@ admin.site.index_title = "Welcome to PERCON Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('Backend.urls')),
+    ] 
     
-]
