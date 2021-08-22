244c308d4a2fb366cd8e1326ee0b4eaed2577acb
diya1511 <diyaagrawal1511@gmail.com>
2021-08-22 09:31:11 +0530

    ok

--- Frontend/Backend/models.py
+++ Frontend/Backend/models.py
@@ -7,6 +7,6 @@ class Contact(models.Model):
     phone=models.CharField(max_length=120)
     desc=models.TextField()
     date=models.DateField()
-    def __str__(self):
-        return self.name
+def __str__(self):
+ return self.name
     
