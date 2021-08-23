6fffd42ba33137aa1eeaefc88e41b2730103ee5b
diya1511 <diyaagrawal1511@gmail.com>
2021-08-23 20:21:27 +0530

    done

--- Frontend/Backend/views.py
+++ Frontend/Backend/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http.response import HttpResponseNotAllowed
 from django.http import HttpResponse
 from .models import *
 from datetime import datetime
 from django.contrib import messages
-
+from django.contrib.auth.models import auth , User
 
 def home(request):
     return render(request,"ind.html")
@@ -22,8 +22,48 @@ def contact(request) :
       contact =Contact(name=name,email=email,phone=phone,desc=desc,date= datetime.today())
       contact.save()
       messages.success(request, 'Your message has been sent!')
-    return render(request,"contact.html")
+    return render(request,"cont.html")
 
 def services(request) :
     return render(request,"services.html")
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2 :
+            if User.objects.filter(email=email).exists():
+                messages.info(request , 'Email already used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request , 'Username Already Used' )
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username , email=email , password=password)
+                user.save();
+                return redirect('login')
+        else:
+            messages.info(request , 'Password is not same')
+            return redirect('register')
+    else:
+        return render(request , 'register.html')
+
+def login(request):
+  if request.method == 'POST':
+           username = request.POST ['username']
+           password = request.POST['password']
+
+           user = auth.authenticate(username=username , password=password)
+
+           if user is not None:
+                 auth.login(request , user)
+                 return redirect('/')
+           else:
+            messages.info(request , 'invalid')
+            return redirect('login')
+            
+  else: 
+             return render ( request , 'login.html')
    
