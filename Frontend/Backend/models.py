55108e6524380325d5e954543a2cc02b41d11249
diya1511 <diyaagrawal1511@gmail.com>
2021-08-25 16:00:55 +0530

    contact done

--- Frontend/Backend/models.py
+++ Frontend/Backend/models.py
@@ -1,10 +1,31 @@
 from django.db import models
+from django.db.models.enums import Choices
+
 class Contact(models.Model):
     name=models.CharField(max_length=200)
-    email=models.EmailField(max_length=120)
     phone=models.CharField(max_length=120)
+    
+    telephone=models.CharField(max_length=120)
+    CHOICES = (
+        ('IN', 'India'),
+        ('US', 'United States'),
+        ('FR', 'France'),
+        ('CN', 'China'),
+        ('RU', 'Russia'),
+        ('IT', 'Italy'),
+        ('OTH', 'Other'),
+    )
+    country = models.CharField(max_length=300, default="India",choices = CHOICES)
+    Choices= (
+        ("F","Female"),
+        ("M","Male"),
+        ("O","Other"),
+    )
+    gender = models.CharField(max_length=130,default="Male",choices= Choices)
+    email=models.EmailField(max_length=120)
     desc=models.TextField()
     date=models.DateField()
+    
     def __str__(self):
        return self.name
     
--- Frontend/Backend/views.py
+++ Frontend/Backend/views.py
@@ -15,11 +15,14 @@ def about(request) :
 def contact(request) :
     if request.method == 'POST' :
       name = request.POST.get('name')
-      email= request.POST.get('email')
       phone = request.POST.get('phone')
+      telephone = request.POST.get('telephone')
+      country= request.POST.get('country')
+      gender= request.POST.get('gender')
+      email= request.POST.get('email')
       desc = request.POST.get('desc')
-    
-      contact =Contact(name=name,email=email,phone=phone,desc=desc,date= datetime.today())
+      
+      contact =Contact(name=name,phone=phone,telephone=telephone,country=country,gender=gender,email=email,desc=desc,date= datetime.today())
       contact.save()
       messages.success(request, 'Your message has been sent!')
     return render(request,"contact.html")
